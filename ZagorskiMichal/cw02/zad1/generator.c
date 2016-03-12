a1d15d998d1060600fb9a4ae61845152c2126c32
Michał Zagórski <zagura6@gmail.com>
2016-03-12 20:43:56 +0100

    Sorting file with system calls

--- ZagorskiMichal/cw02/zad1/generator.c
+++ ZagorskiMichal/cw02/zad1/generator.c
@@ -6,21 +6,34 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 int main(int argc, char** argv){
+	int seed = time(NULL);
+	srand(seed);
 	if(argc != 3){
 		printf("Wrong number of arguments\n");
 		return 0;
 	}
-
 	char* filename = argv[0];
 	char* block_size_s = argv[1];
 	char* records_count_s = argv[2];
 
-	unsigned long block_size = atol(block_size_s);
-	unsigned long long records_count = atoq(records_count_s);
+	unsigned long record_size = atol(block_size_s);
+	unsigned long long records_count = atoll(records_count_s);
 	FILE* file = fopen(filename, "w");
+	if(file == NULL){
+		perror("Error with file.\n");
+		exit(1);
+	}
+	char* record = calloc(record_size, sizeof(char));
+	for(unsigned long long index = 0; index < records_count; index++){
 
-
+	}
+	if(file !=  NULL){
+		fclose(file);
+	}
+	free(record);
 	return 0;
 }
--- ZagorskiMichal/cw02/zad1/readfile.c
+++ ZagorskiMichal/cw02/zad1/readfile.c
@@ -11,7 +11,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 
+	
 int compare_records(char* rec1, char* rec2);
 
 void insertion_sort(char* filename, int rec_size, char type);
@@ -22,7 +25,10 @@ void insertion_sort(char* filename, int rec_size, char type);
 
 
 
+int main(int argc, char** argv){
 
+	return 0;
+}
 
 
 
@@ -71,7 +77,7 @@ int lib_sort(const char* pathname, long record_size){
 	char* record = NULL;
 	char* cur_record = NULL;
 	if(file == NULL){
-		perror("Can't open file %s.", pathname);
+		perror("Can't open file.\n");
 		return -2;
 	}else{
 		if(fseek(file, 0, SEEK_END) == -1){
@@ -88,17 +94,20 @@ int lib_sort(const char* pathname, long record_size){
 			perror("Wrong size of file... Not integer count of records");
 		}
 		for(long i = 1; i < records_count; i++){
-			last_offset = i*record_size;
+			long last_offset = i*record_size + begin_offset;
 			if(fseek(file, last_offset, SEEK_SET) == -1){
 				goto lib_io_error;
 			}//			if(last_offset < end_offset){
 			cur_record = calloc(record_size, sizeof(char));
+			if(fread(cur_record, record_size, sizeof(char), file) != record_size*sizeof(char)){
+				goto lib_io_error;
+			}
 			bool insert_flag = true;
 			for(long offset = (i-1)*record_size; 
 				offset >= begin_offset && insert_flag; 
-				ofsset -= record_size){
+				offset -= record_size){
 				record = calloc(record_size, sizeof(char));
-				if(!fread(record, record_size, 1, file)){
+				if(fread(record, record_size, sizeof(char), file) != record_size*sizeof(char)){
 					goto lib_io_error;
 				}if(compare_records(cur_record, record) < 1){
 					/* case, when current record <= record in sorted fragment
@@ -107,7 +116,7 @@ int lib_sort(const char* pathname, long record_size){
 					if(fseek(file, record_size, SEEK_CUR) == -1){
 						goto lib_io_error;
 					}
-					if(fwrite(record, record_size, sizof(char), file) 
+					if(fwrite(record, record_size, sizeof(char), file) 
 						!= record_size*sizeof(char)){
 						goto lib_io_error;
 					}
@@ -124,7 +133,7 @@ int lib_sort(const char* pathname, long record_size){
 					if(fseek(file, record_size, SEEK_CUR) == -1){
 						goto lib_io_error;
 					}
-					if(fwrite(cur_record, record_size, sizof(char), file) 
+					if(fwrite(cur_record, record_size, sizeof(char), file) 
 						!= record_size*sizeof(char)){
 						goto lib_io_error;
 					}
@@ -137,13 +146,16 @@ int lib_sort(const char* pathname, long record_size){
 			}
 			if(cur_record != NULL){
 				free(cur_record);
-				cur_record = NULL:
+				cur_record = NULL;
 			}
 		}
 	}	
 /*	lib_no_error: */
 	if(file != NULL){
-		fclose(file);
+		if(fclose(file) != 0){
+			goto lib_io_error;
+		}
+		file = NULL;
 	}
 	if(record != NULL){
 		free(record);
@@ -157,6 +169,7 @@ int lib_sort(const char* pathname, long record_size){
 		perror("Error during work with file.\n");
 		if(file != NULL){
 			fclose(file);
+			file = NULL;
 		}
 		if(record != NULL){
 			free(record);
@@ -166,92 +179,104 @@ int lib_sort(const char* pathname, long record_size){
 		}
 		return -1;
 }
-
-
+/*
+	sys_sort - sorting file named in pathname, using system calls to communicate with file
+*/
 int sys_sort(const char* pathname, long record_size){
 	int file = open(pathname, O_RDWR);
 	char* record = NULL;
 	char* cur_record = NULL;
 	if(file == -1){
-		perror("Can't open file %s.", pathname);
+		perror("Can't open file.\n");
 		return -2;
 	}else{
-		off_t end_offset = lseek(file, 0, SEEK_END);
 		off_t begin_offset = lseek(file, 0, SEEK_SET);
-		if(end_offset == -1 || begin_offset == -1){
+		off_t end_offset = lseek(file, 0, SEEK_END);
+		if(begin_offset == -1 || end_offset == -1){
 			goto sys_io_error;
 		}
-		while(true){
-			int seek_error = lseek(file, record_size, SEEK_CUR);
-			if(seek_error == -1){
+		off_t file_size = end_offset - begin_offset;
+		long records_count = file_size / record_size;
+		if(file_size % record_size == 0){
+			perror("Wrong size of file... Not integer count of records");
+		}
+		for(long i = 1; i < records_count; i++){
+			long last_offset = i*record_size;
+
+			if(lseek(file, last_offset, SEEK_SET) == -1){
 				goto sys_io_error;
-			}else{
-				if(seek_error < end_offset)){
-					cur_record = calloc(record_size, sizeof(char));
-					bool insert_flag = true;
-					do{
-						seek_error = seek(file, (-1)*record_size, SEEK_CUR);
-						if(seek_error == -1 || tell(file) < 0L){
+			}//			if(last_offset < end_offset){
+			cur_record = calloc(record_size, sizeof(char));
+			if(read(file, cur_record, record_size*sizeof(char)) != record_size*sizeof(char)){
+				goto sys_io_error;
+			}
+			bool insert_flag = true;
+			for(long offset = (i-1)*record_size + begin_offset; 
+				offset >= begin_offset && insert_flag; 
+				offset -= record_size){
+				record = calloc(record_size, sizeof(char));
+				if(read(file, record, record_size*sizeof(char)) != record_size*sizeof(char)){
+					goto sys_io_error;
+				}if(compare_records(cur_record, record) < 1){
+					/* case, when current record <= record in sorted fragment
+						moving record from sorted fragment
+					*/
+					if(lseek(file, record_size, SEEK_CUR) == -1){
+						goto sys_io_error;
+					}
+					if(write(file, record, record_size*sizeof(char)) != record_size*sizeof(char)){
+						goto sys_io_error;
+					}
+					if(offset == begin_offset){
+						if(lseek(file, record_size, SEEK_CUR) == -1){
 							goto sys_io_error;
-						}else{
-							record = calloc(record_size, sizeof(char));
-							if(!read(record, record_size, file)){
-								goto sys_io_error;
-							}if(lseek(file, record_size, SEEK_CUR) == -1){
-								goto sys_io_error;
-							}
-							if(compare_records(cur_record, record) == 1){
-								if(write(record, record_size, file) != record_size){
-									goto sys_io_error;
-								}
-							}else{
-								insert_flag = false;
-							}
-							if(record != NULL){
-								free(record);
-							}
-							record = NULL;
 						}
-					}while(ftell(file) > 0L && insert_flag);
-					if(insert_flag){
-						if(lseek(file, (-1)*record_size, SEEK_CUR) == -1){
+						if(write(file, cur_record, record_size*sizeof(char)) 
+							!= record_size*sizeof(char)){
 							goto sys_io_error;
 						}
 					}
-					if(write(cur_record, record_size, file) != record_size){
+				}else{
+					if(lseek(file, record_size, SEEK_CUR) == -1){
 						goto sys_io_error;
 					}
-					if(cur_record != NULL){
-						free(cur_record);
-					}
-					if(lseek(file, offset, SEEK_SET) == -1){
+					if(write(file, cur_record, record_size*sizeof(char)) 
+						!= record_size*sizeof(char)){
 						goto sys_io_error;
 					}
+					insert_flag = false;
 				}
+				if(record != NULL){
+					free(record);
+					record = NULL;
+				}
+			}
+			if(cur_record != NULL){
+				free(cur_record);
+				cur_record = NULL;
 			}
 		}
 	}	
 /*	sys_no_error: */
-		if(file != -1){
-			if(close(file) == -1){
-				perror("Error closing file.\n");
-				goto sys_io_error;
-			}
+	if(file != 0){
+		if(close(file) != 0){
+			goto sys_io_error;
 		}
-		if(record != NULL){
-			free(record);
-		}
-		if(cur_record != NULL){
-			free(cur_record);
-		}
-		return 0;
+		file = 0;
+	}
+	if(record != NULL){
+		free(record);
+	}
+	if(cur_record != NULL){
+		free(cur_record);
+	}
+	return 0;
 
 	sys_io_error:
 		perror("Error during work with file.\n");
-		if(file != -1){
-			if(close(file) == -1){
-				perror("Error closing file.\n");
-			}
+		if(file != 0){
+			close(file);
+			file = 0;
 		}
 		if(record != NULL){
 			free(record);
